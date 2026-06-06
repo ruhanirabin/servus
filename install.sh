d1633d0f2e29871f64816786308a3a42e8984017
Ruhani Rabin <get@ruhanirabin.com>
2026-06-07 00:56:27 +0800

    (fix): color escape codes rendering

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ CONF_DIR="/usr/local/etc/servus"
 LOG_DIR="/var/log/servus"
 STATE_DIR="/var/lib/servus"
 
-RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; CYAN='\033[0;36m'; BOLD='\033[1m'; NC='\033[0m'
+RED=$'\033[0;31m'; GREEN=$'\033[0;32m'; YELLOW=$'\033[1;33m'; CYAN=$'\033[0;36m'; BOLD=$'\033[1m'; NC=$'\033[0m'
 info()    { echo -e "${CYAN}[servus]${NC} $*"; }
 success() { echo -e "${GREEN}[servus]${NC} $*"; }
 warn()    { echo -e "${YELLOW}[servus]${NC} $*"; }
--- lib/common.sh
+++ lib/common.sh
@@ -7,12 +7,12 @@ SERVUS_CONF_FILE="/usr/local/etc/servus/config"
 SERVUS_LOG_FILE="/var/log/servus/servus.log"
 SERVUS_STATE_DIR="/var/lib/servus"
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-CYAN='\033[0;36m'
-BOLD='\033[1m'
-NC='\033[0m'
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+CYAN=$'\033[0;36m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
 
 info()    { echo -e "${CYAN}[servus]${NC} $*"; }
 success() { echo -e "${GREEN}[servus]${NC} $*"; }
